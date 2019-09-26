6833ff45d7fdc96806711f8fc38631e0b7d49f01
Daria Hauer <dariahauer2@gmail.com>
2019-09-26 12:48:03 +0200

    Zadanie domowe poprawa dla sprawdzenia bo pominęłam push.

--- src/ZadanieDomowe1.java
+++ src/ZadanieDomowe1.java
@@ -4,8 +4,7 @@
             System.out.println("sie");
             System.out.println("Programowania");
             System.out.println("i");
-            System.out.println("commitowania.");
-            System.out.println("Mam nadzieje, ze sie duzo naucze.");
+            System.out.println("commitowania");
         }
     }
 
